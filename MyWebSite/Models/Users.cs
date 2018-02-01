46b570b4eb850c034f1a7b910786db01d8716319
luomilande <luomilande@WB19>
2018-02-01 16:44:27 +0800

    添加项目文件。

---
+++ MyWebSite/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebSite.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
